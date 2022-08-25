a373945035954baf6d32c503bb0acb1db75b0d13
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-25 07:47:43 -0400

    Initial commit

---
+++ src/main/java/Countdown.java
@@ -0,0 +1,21 @@
+public class Countdown {
+
+    public static void main(String[] args) {
+
+        System.out.println("Countdown to Launch: ");
+
+        for(int i =0; i <= 5; i++) {
+            System.out.print(i +" ");
+        }
+
+        System.out.println("Blast Off!\n\n");
+
+        System.out.println("Countdown to Launch: ");
+
+        for(int i =0; i <= 20; i+=2) {
+            System.out.print(i +" ");
+        }
+
+        System.out.println("Blast Off!");
+    }
+}
